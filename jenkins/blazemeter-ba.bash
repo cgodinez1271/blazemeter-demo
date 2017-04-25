32d21fcf6de7d81b493a04a6cf19eb9d84e22c29
zmicer <dzmitrykashlach@gmail.com>
2017-04-25 12:44:07 +0300

    blazemeter-ba.sh

--- jenkins/blazemeter-ba.bash
+++ jenkins/blazemeter-ba.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export BZA=https://a.blazemeter.com
-export TEST_ID="test-id"
-export USER="id:secret"
+export TEST_ID="5597665"
+export USER="d8b2950468ff21a4df902f3f:6da71a14ebdc612cb7405eac184c9958d1f6d8c384e82e0fe8811c352a06f5fa0ff3a6e1"
 
 echo "Running test on " $BZA
 echo "Test ID = " $TEST_ID
